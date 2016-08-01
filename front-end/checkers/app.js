d8bb337eff7154cc2962f11d51916f16fde5acef
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-08-01 11:22:10 -0700

    completed checker problem

--- front-end/checkers/app.js
+++ front-end/checkers/app.js
@@ -2,17 +2,37 @@ function renderBox(color){
   $("body").append("<div class=" + color + "></div>");
 }
 
-function line(num) {
+function horizontalBR(num){
   for (var i = 0; i < num; i++){
     if(i % 2 === 0){
-      renderBox('black');
+      renderBox('blackIn');
     } else {
-      renderBox('red');
+      renderBox('redIn');
     }
   }
 }
-// $(document).ready(function() {
-  // $( "body" ).append( "<p>Test</p>" );
-// });
+function horizontalRB(num){
+  for (var i = 0; i < num; i++){
+    if(i % 2 === 0){
+      renderBox('redIn');
+    } else {
+      renderBox('blackIn');
+    }
+  }
+}
+
+function line(num) {
+  for(var j = 0; j < num; j++){
+    if(j % 2 === 0){
+      horizontalBR(num);
+    } else {
+      horizontalRB(num);
+    }
+    $("body").append("<br />")
+  }
+}
+
 
 line(10);
+
+//line(width, length)
